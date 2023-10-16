4e21915a1697d6f8b32e00b0607c3a980ef2283c
josiahmattern <98362399+josiahmattern@users.noreply.github.com>
2023-10-16 14:12:19 -0400

    inital commit

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,16 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use tauri::generate_handler;
+
+fn main() {
+  tauri::Builder::default()
+  .invoke_handler(generate_handler![greet])
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn greet(name: &str) -> String {
+  format!("Hello, {}!", name)
+}
